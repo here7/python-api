5e31b244c80cb1219b3cb52635e82c2adadedc59
dani heredia <daniel.heredia@manomano.com>
2024-02-13 17:17:16 +0100

    Oauth included with fake DB

---
+++ app/routers/basic_auth_users.py
@@ -0,0 +1,76 @@
+from fastapi import Depends, FastAPI, HTTPException, status
+from pydantic import BaseModel
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+app = FastAPI()
+oauth2 = OAuth2PasswordBearer(tokenUrl="login")
+
+
+class User(BaseModel):
+    username: str
+    full_name: str
+    email: str
+    disabled: bool
+
+class UserDB(User):
+    password: str
+
+#simulating a DB
+users_db = {
+    "dani_1": {
+        "username": "dani_1",
+        "full_name": "Dani Heredia",
+        "email": "dani@gmail.com",
+        "disabled": False,
+        "password": "123456" 
+    },
+        "dani_2": {
+        "username": "dani_2",
+        "full_name": "Dani Heredia 2",
+        "email": "dani2@gmail.com",
+        "disabled": True,
+        "password": "654321" 
+    }
+}
+
+
+def search_user(username: str):
+    if username in users_db:
+        return UserDB(users_db[username])
+
+
+ # criterio de dependecia para /me (por eso es async tb)
+async def current_user(token: str = Depends(oauth2)):
+    user = search_user(token)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail="Not valid Credentials",
+            headers={"WWW-Authenticate": "Bearer"}
+        )
+
+# authentication implementation
+@app.post("/login")
+async def login(form: OAuth2PasswordRequestForm = Depends()):
+    user_db = users_db.get(form.username)
+    if not user_db:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="error: user wasn't correct")
+    
+    user = search_user(form.username)
+
+    # check if passwd in the form coincides with my user
+    if not form.password == user.password:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="error: password not correct")
+    
+    # if all good, return access_token (example with no encryption)
+    return {"access_token": user.username, "token_type": "bearer"}
+    
+
+@app.get("/users/me")
+async def  me(user: User = Depends(current_user)):
+    return user
