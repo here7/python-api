0797aa4964e440fef8dc6c60a8bc1d419dc4d0d5
dani heredia <daniel.heredia@manomano.com>
2024-02-13 19:45:03 +0100

    JWT auth

--- app/routers/jwt_auth_users.py
+++ app/routers/jwt_auth_users.py
@@ -1,13 +1,13 @@
 from fastapi import Depends, FastAPI, HTTPException, status
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import jwt
+from jose import jwt, JWTError
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 
-
+SECRET_KEY = "3ec6dcb3460c3d9957dbd7aacf4de6b7dda6d8b690f9bf00265d6499c79a1ee2"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_DURATION = 1
+ACCESS_TOKEN_DURATION = 5
 
 app = FastAPI()
 oauth2 = OAuth2PasswordBearer(tokenUrl="login")
@@ -40,12 +40,43 @@ users_db = {
     }
 }
 
+# ** -> indicates that the BaseModel could have multiple params
+# data with no passwd
+def search_user(username: str):
+    if username in users_db:
+        return User(**users_db[username])
+
 
 # with passwd
 def search_user_db(username: str):
     if username in users_db:
         return UserDB(**users_db[username])
 
+# Token Validation function
+async def auth_user(token: str = Depends(oauth2)):
+    exception = HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED, 
+                    detail="Not valid Credentials",
+                    headers={"WWW-Authenticate": "Bearer"}
+                )
+    try:
+        username = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM]).get("sub")
+        if username is None:
+            raise exception
+    except JWTError:
+        raise exception
+    
+    return search_user(username)
+
+# criterio de dependecia para /me (por eso es async tb)
+async def current_user(user: User = Depends(auth_user)):  
+    if user.disabled:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, 
+            detail="User disabled"
+        ) 
+    
+    return user
 
 # authentication implementation
 @app.post("/login")
@@ -69,4 +100,8 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
                     "exp": datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_DURATION) }
 
     # if all good, return access_token (example with no encryption)
-    return {"access_token": access_token, "token_type": "bearer"}
+    return {"access_token": jwt.encode(access_token, SECRET_KEY, algorithm=ALGORITHM), "token_type": "bearer"}
+
+@app.get("/users/me")
+async def me(user: User = Depends(current_user)):
+    return user
